3da0606fccd83f28997f4e7f55ed14e17181fcf5
veereshkumar <veereshkumar.b.s18@gmail.com>
2025-03-13 20:56:59 +0530

    commit 3

---
+++ MyProject/src/OPPs/demo/Enca.java
@@ -0,0 +1,31 @@
+package OPPs.demo;
+
+public class Enca {
+	
+	private int id;
+	private int age;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Enca [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+	
+}
---
+++ MyProject/src/OPPs/demo/Encap_Main.java
@@ -0,0 +1,22 @@
+package OPPs.demo;
+
+public class Encap_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Enca obj=new Enca();
+		
+		obj.setId(1);
+		obj.setName("Veeresh");
+		obj.setAge(21);
+		
+		System.out.println(obj);
+		
+		obj.setId(2);
+		obj.setName("Arun");
+		obj.setAge(22);
+		
+		System.out.println(obj);
+	}
+
+}
